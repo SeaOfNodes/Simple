2f9d81d0b3b2db584a1c3e09984cdd707581791d
Dibyendu Majumdar <mobile@majumdar.org.uk>
2023-08-19 08:52:57 -0700

    Chapter 1: Introduction
    Co-authored-by: Cliff Click <cliffc@acm.org>
    Co-authored-by: Thalia Archibald <thalia@archibald.dev>
    Co-authored-by: hadha <helsgithub15@gmail.com>
    Co-authored-by: XmiliaH <45106915+XmiliaH@users.noreply.github.com>
    Co-authored-by: Tamás Hadházy <85063808+Hels15@users.noreply.github.com>

---
+++ transpile-tests/src/main/java/com/seaofnodes/simple/transpile/TestMethod.java
@@ -0,0 +1,39 @@
+package com.seaofnodes.simple.transpile;
+
+import java.util.ArrayList;
+
+public class TestMethod {
+    public String name;
+    public String parserInput;
+    public Arg parserArg;
+    // if this is set you can ignore the rest
+    public String parseErrorMessage;
+    public boolean disablePeephole;
+    public boolean showAfterParse;
+    public boolean iterate;
+    public boolean showAfterIterate;
+    public boolean irPrinter;
+    public boolean irPrinterLLVM;
+    public String assertStopEquals;
+    public boolean assertStopRetCtrlIsProj;
+    public boolean assertStopRetCtrlIsCProj;
+    public boolean assertStopRetCtrlIsRegion;
+    public final ArrayList<Evaluation> evaluations = new ArrayList<>();
+
+    public sealed interface Arg {
+        record IntBot() implements Arg {
+        }
+
+        record IntTop() implements Arg {
+        }
+
+        record IntConstant(long value) implements Arg {
+        }
+    }
+
+    /**
+     * result is a literal value
+     **/
+    public record Evaluation(Object result, long parameter) {
+    }
+}
