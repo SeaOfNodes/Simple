8e8dcf86abfdd8d90b11ede7db2e94666f4b7356
Dibyendu Majumdar <mobile@majumdar.org.uk>
2023-08-19 08:52:57 -0700

    Chapter 1: Introduction
    Co-authored-by: Cliff Click <cliffc@acm.org>
    Co-authored-by: Thalia Archibald <thalia@archibald.dev>
    Co-authored-by: hadha <helsgithub15@gmail.com>
    Co-authored-by: XmiliaH <45106915+XmiliaH@users.noreply.github.com>
    Co-authored-by: Tamás Hadházy <85063808+Hels15@users.noreply.github.com>

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+(
+ (dired-mode . ((c-basic-offset . 4)))
+ (java-mode  . ((c-basic-offset . 4)))
+)
