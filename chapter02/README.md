b7d3e9297128ccf3e783140c6c722f77a36961fd
dibyendumajumdar <mobile@majumdar.org.uk>
2023-09-23 14:41:26 +0100

    Start on graph visualizer

--- chapter02/README.md
+++ chapter02/README.md
@@ -1,4 +1,4 @@
-# Chapter 1
+# Chapter 2
 
 In this chapter we extend the language grammar to include arithmetic operations such as addition, subtraction,
 multiplication, division, and unary minus. This allows us to write statements such as:
@@ -9,3 +9,12 @@ return 1 + 2 * 3 + -5;
 
 Here is the [complete language grammar](docs/02-grammar.md) for this chapter. 
 
+## Nodes Pre Peephole Optimization
+
+The following visual shows how the graph looks like pre-peephole optimization:
+
+![Example Visual](./docs/02-pre-peephole-ex1.svg)
+
+* Control nodes appear as square boxes
+* Control edges are in bold red
+* The edges from Start to Constants are shown in dotted lines as these are not true def-use edges
--- chapter02/src/main/java/com/seaofnodes/simple/GraphVisualizer.java
+++ chapter02/src/main/java/com/seaofnodes/simple/GraphVisualizer.java
@@ -5,76 +5,74 @@ import com.seaofnodes.simple.node.Control;
 import com.seaofnodes.simple.node.Node;
 import com.seaofnodes.simple.node.StartNode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GraphVisualizer {
 
-    Map<Integer, Node> _controls = new HashMap<>();
-    Map<Integer, Node> _dataNodes = new HashMap<>();
+    Map<Integer, Node> _all = new HashMap<>();
 
     public String generateDotOutput(StartNode start) {
         for (int i = 0; i < start.nOuts(); i++) {
             Node n = start.out(i);
-            if (!(n instanceof ConstantNode)) {
-                walk(n);
-            }
+            walk(n);
         }
         // We should have all nodes in our two maps.
         StringBuilder sb = new StringBuilder();
         sb.append("digraph chapter01\n" +
                 "{\n");
-        for (Node n : _dataNodes.values()) {
-            sb.append('\t').append(n.uniqueName())
-                    .append(" [label=\"")
+        for (Node n : _all.values()) {
+            sb.append('\t').append(n.uniqueName());
+            // control nodes have box shape
+            // other nodes are ellipses, i.e. default shape
+            if (n instanceof Control)
+                sb.append(" [shape=box, ");
+            else
+                sb.append(" [");
+            sb.append(" label=\"")
                     .append(n.label())
                     .append("\"];\n");
-            walkIns(sb, n);
+            // Output the non control edges
+            walkIns(sb, n, false);
         }
+        // Now output the control edges which must be in red
         sb.append("\tedge [color=red];\n");
-        for (Node n : _controls.values()) {
-            sb.append('\t').append(n.uniqueName())
-                    .append(" [shape=box, label=\"")
-                    .append(n.label())
-                    .append("\"];\n");
-            walkIns(sb, n);
+        for (Node n : _all.values()) {
+            walkIns(sb, n, true);
         }
         sb.append("}\n");
         return sb.toString();
     }
 
-    private void walkIns(StringBuilder sb, Node n) {
-        for (int i = 0; i < n.nIns(); i++) {
-            Node out = n.in(i);
+    private void walkIns(StringBuilder sb, Node in, boolean doControlEdges) {
+        for (int i = 0; i < in.nIns(); i++) {
+            Node out = in.in(i);
             if (out == null)
                 continue;
-            sb.append('\t').append(out.uniqueName())
+            boolean isControlEdge = (out instanceof Control) && (in instanceof Control);
+            if (doControlEdges && !isControlEdge)
+                continue;
+            if (!doControlEdges && isControlEdge)
+                continue;
+            sb.append('\t')
+                    .append(out.uniqueName())
                     .append(" -> ")
-                    .append(n.uniqueName())
-                    .append(";\n");
+                    .append(in.uniqueName());
+            // the edge from start node to constants is just for convenience so
+            // show it differently
+            if ((out instanceof StartNode) && (in instanceof ConstantNode))
+                sb.append(" [style=dotted]");
+            sb.append(";\n");
         }
     }
 
     private void walk(Node n) {
-        Map<Integer, Node> map;
-        List<Node> nodesToVisit = new ArrayList<>();
-        if (n instanceof Control) {
-            map = _controls;
-        } else {
-            map = _dataNodes;
-        }
-        if (map.get(n._nid) == null) {
+        if (_all.get(n._nid) == null) {
             // Not yet seen
-            map.put(n._nid, n);
-            nodesToVisit.add(n);
+            _all.put(n._nid, n);
+            for (Node c : n._inputs)
+                if (c != null)
+                    walk(c);
         }
-        for (Node c : n._inputs) {
-            if (c != null)
-                walk(c);
-        }
-        for (Node c : nodesToVisit)
-            walk(c);
     }
 }
