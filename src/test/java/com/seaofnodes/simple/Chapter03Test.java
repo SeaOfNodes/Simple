af5b28e0e5c839b9af0adbeec693bf929a66507b
Dibyendu Majumdar <mobile@majumdar.org.uk>
2023-10-01 18:01:54 +0100

    Chapter 3: Variable Declarations
    Co-authored-by: Cliff Click <cliffc@acm.org>
    Co-authored-by: Yichen Yan <oraluben@outlook.com>
    Co-authored-by: Thalia Archibald <thalia@archibald.dev>
    Co-authored-by: Tamas Hadhazy <helsgithub15@gmail.com>
    Co-authored-by: XmiliaH <45106915+XmiliaH@users.noreply.github.com>
    Co-authored-by: Tamás Hadházy <85063808+Hels15@users.noreply.github.com>
    Co-authored-by: Hsiang-Ying Fu <66062782+fuhsnn@users.noreply.github.com>
    Co-authored-by: tannal <tannal2409@gmail.com>
    Co-authored-by: hadha <helsgithub15@gmail.com>
    Co-authored-by: godalming123 <68993177+godalming123@users.noreply.github.com>
    Co-authored-by: RobertObkircher <robert.obkircher@gmail.com>

---
+++ src/test/java/com/seaofnodes/simple/Chapter03Test.java
@@ -0,0 +1,77 @@
+package com.seaofnodes.simple;
+
+import com.seaofnodes.simple.node.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Chapter03Test {
+
+    @Test
+    public void testVarDecl() {
+        Parser parser = new Parser("int a=1; return a;");
+        ReturnNode ret = parser.parse();
+        assertEquals("return 1;", ret.print());
+    }
+
+    @Test
+    public void testVarAdd() {
+        Parser parser = new Parser("int a=1; int b=2; return a+b;");
+        ReturnNode ret = parser.parse();
+        assertEquals("return 3;", ret.print());
+    }
+
+    @Test
+    public void testVarScope() {
+        Parser parser = new Parser("int a=1; int b=2; int c=0; { int b=3; c=a+b; } return c;");
+        ReturnNode ret = parser.parse();
+        assertEquals("return 4;", ret.print());
+    }
+
+    @Test
+    public void testVarScopeNoPeephole() {
+        Parser parser = new Parser("int a=1; int b=2; int c=0; { int b=3; c=a+b; #showGraph; } return c; #showGraph;");
+        Node._disablePeephole = true;
+        ReturnNode ret = parser.parse();
+        Node._disablePeephole = false;
+        assertEquals("return (1+3);", ret.print());
+    }
+
+    @Test
+    public void testVarDist() {
+        Parser parser = new Parser("int x0=1; int y0=2; int x1=3; int y1=4; return (x0-x1)*(x0-x1) + (y0-y1)*(y0-y1); #showGraph;");
+        ReturnNode ret = parser.parse();
+        assertEquals("return 8;", ret.print());
+    }
+
+    @Test
+    public void testSelfAssign() {
+        try {
+            new Parser("int a=a; return a;").parse();
+            fail();
+        } catch( RuntimeException e ) {
+            assertEquals("Undefined name 'a'",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testRedeclareVar() {
+        try {
+            new Parser("int a=1; int b=2; int c=0; int b=3; c=a+b;").parse();
+            fail();
+        } catch( RuntimeException e ) {
+            assertEquals("Redefining name 'b'",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBad1() {
+        try {
+            new Parser("int a=1; int b=2; int c=0; { int b=3; c=a+b;").parse();
+            fail();
+        } catch( RuntimeException e ) {
+            assertEquals("Syntax error, expected }: ",e.getMessage());
+        }
+    }
+
+}
