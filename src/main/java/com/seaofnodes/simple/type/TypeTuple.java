7cd000c24e2269bf3154594480219bee6111099a
Dibyendu Majumdar <mobile@majumdar.org.uk>
2023-10-20 15:21:08 -0700

    Chapter 4: External Argument and Comparisons
    Co-authored-by: Cliff Click <cliffc@acm.org>
    Co-authored-by: Yichen Yan <oraluben@outlook.com>
    Co-authored-by: Tamas Hadhazy <helsgithub15@gmail.com>
    Co-authored-by: Hels15 <helsgithub15@gmail.com>
    Co-authored-by: XmiliaH <45106915+XmiliaH@users.noreply.github.com>
    Co-authored-by: Thalia Archibald <thalia@archibald.dev>

---
+++ src/main/java/com/seaofnodes/simple/type/TypeTuple.java
@@ -0,0 +1,26 @@
+package com.seaofnodes.simple.type;
+
+public class TypeTuple extends Type {
+
+    public final Type[] _types;
+
+    public TypeTuple(Type... _types) {
+        super(TTUPLE);
+        this._types = _types;
+    }
+
+    @Override
+    public Type meet(Type other) {
+        throw new UnsupportedOperationException("Meet on Tuple Type not yet implemented");
+    }
+
+    @Override
+    public StringBuilder _print(StringBuilder sb) {
+        sb.append("[ ");
+        for( Type t : _types )
+            t._print(sb).append(",");
+        sb.setLength(sb.length()-1);
+        sb.append("]");
+        return sb;
+    }
+}
