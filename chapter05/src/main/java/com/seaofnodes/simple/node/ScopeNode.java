ae27075cf99de931f91f3ebd26e19fe47fa8447c
Cliff Click <cliffc@acm.org>
2023-10-28 14:23:48 -0700

    Dont make redundant phis

--- chapter05/src/main/java/com/seaofnodes/simple/node/ScopeNode.java
+++ chapter05/src/main/java/com/seaofnodes/simple/node/ScopeNode.java
@@ -116,7 +116,8 @@ public class ScopeNode extends Node {
     public RegionNode mergeScopes(ScopeNode that) {
         RegionNode r = (RegionNode)ctrl(new RegionNode(ctrl(), that.ctrl()).peephole());
         for( int i=1; i<nIns(); i++ )
-            set_def(i,new PhiNode(r, in(i), that.in(i)).peephole());
+            if( in(1) != that.in(i) ) // No need for redundant Phis
+                set_def(i,new PhiNode(r, in(i), that.in(i)).peephole());
         that.kill();            // Kill merged scope
         return r;
     }
