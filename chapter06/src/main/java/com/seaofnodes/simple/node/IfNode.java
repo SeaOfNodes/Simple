66701ef46e954b4ef12cd626a06dddc609055449
dibyendumajumdar <mobile@majumdar.org.uk>
2023-11-04 12:48:15 +0000

    #37 Revise compute of if

--- chapter06/src/main/java/com/seaofnodes/simple/node/IfNode.java
+++ chapter06/src/main/java/com/seaofnodes/simple/node/IfNode.java
@@ -1,6 +1,7 @@
 package com.seaofnodes.simple.node;
 
 import com.seaofnodes.simple.type.Type;
+import com.seaofnodes.simple.type.TypeInteger;
 import com.seaofnodes.simple.type.TypeTuple;
 
 public class IfNode extends MultiNode {
@@ -19,10 +20,23 @@ public class IfNode extends MultiNode {
     }
 
     @Override public boolean isCFG() { return true; }
-    
+
+    public Node ctrl() { return in(0); }
+    public Node pred() { return in(1); }
+
     @Override
     public Type compute() {
-        return TypeTuple.IF;
+        // If the If node is not reachable then neither is any following Proj
+        if (ctrl()._type != Type.CONTROL) return TypeTuple.IF_BOTH_UNREACHABLE;
+        // If test os not a constant true/false then assume both branches are reachable
+        if (!pred()._type.isConstant())   return TypeTuple.IF_BOTH_REACHABLE;
+        // If constant is 0 then false branch is reachable
+        // Else true branch is reachable
+        if (pred()._type instanceof TypeInteger ti) {
+            if (ti.value() == 0)          return TypeTuple.IF_FALSE_REACHABLE;
+            else                          return TypeTuple.IF_TRUE_REACHABLE;
+        }
+        throw new IllegalStateException("Unexpected predicate type " + pred()._type);
     }
 
     @Override
