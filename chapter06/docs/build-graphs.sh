d481b1007b7ba5ae09bb665ae993d7fc633e6ba6
dibyendumajumdar <mobile@majumdar.org.uk>
2023-11-18 16:29:42 +0000

    #40 Revise chapter 6 writeup to include a description of dominators

--- chapter06/docs/build-graphs.sh
+++ chapter06/docs/build-graphs.sh
@@ -3,6 +3,7 @@ dot -Tsvg  06-graph1-post.gv > 06-graph1-post.svg
 dot -Tsvg  06-graph2-pre.gv > 06-graph2-pre.svg
 dot -Tsvg  06-graph2-post.gv > 06-graph2-post.svg
 dot -Tsvg  06-graph3-nonconst.gv > 06-graph3-nonconst.svg
+dot -Tsvg  06-graph3-nonconst-peephole.gv > 06-graph3-nonconst-peephole.svg
 dot -Tsvg  06-graph3-true.gv > 06-graph3-true.svg
 dot -Tsvg  06-graph3-false.gv > 06-graph3-false.svg
 dot -Tsvg  06-graph4-nonconst.gv > 06-graph4-nonconst.svg
