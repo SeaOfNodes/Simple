86ed762656293279ef0e112021f5f8fb3f12e5d2
dibyendumajumdar <mobile@majumdar.org.uk>
2024-03-30 11:50:29 +0000

    #67 Use the generated graph but an edge is missing in the graph - to be investigated

--- chapter10/docs/build-graphs.sh
+++ chapter10/docs/build-graphs.sh
@@ -1,6 +1,1 @@
-dot -Tsvg  09-graph1.gv > 09-graph1.svg
-dot -Tsvg  09-graph2.gv > 09-graph2.svg
-dot -Tsvg  09-graph3.gv > 09-graph3.svg
-dot -Tsvg  09-graph4.gv > 09-graph4.svg
-dot -Tsvg  09-graph5.gv > 09-graph5.svg
-dot -Tsvg  09-graph6.gv > 09-graph6.svg
+dot -Tsvg  example1.gv > example1.svg
