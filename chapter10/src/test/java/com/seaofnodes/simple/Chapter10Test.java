021909d6612d7a6d839620586b77ac45fb23ad04
dibyendumajumdar <mobile@majumdar.org.uk>
2024-04-28 20:20:50 +0100

    #67 Update visuals. Add description of Store-Store and Store-Load peeps. Add examples.

--- chapter10/src/test/java/com/seaofnodes/simple/Chapter10Test.java
+++ chapter10/src/test/java/com/seaofnodes/simple/Chapter10Test.java
@@ -28,7 +28,7 @@ Foo? foo = null;
 Bar bar = new Bar;
 bar.a = 1;
 bar.a = 2;
-return bar.a;
+return bar;
 """);
         StopNode stop = parser.parse(false).iterate(true);
         System.out.println(IRPrinter.prettyPrint(stop, 99, true));
