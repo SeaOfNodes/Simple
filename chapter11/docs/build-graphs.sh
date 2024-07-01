6939672f7145f862b55a5fbd6d53137f4a85b393
Thalia Archibald <thalia@archibald.dev>
2024-06-30 23:28:08 -0700

    Forward-port replace build-graphs.sh with Makefile

--- chapter11/docs/build-graphs.sh
+++
@@ -1,9 +0,0 @@
-dot -Tsvg  graph1.gv > graph1.svg
-dot -Tsvg  graph2.gv > graph2.svg
-dot -Tsvg  graph3.gv > graph3.svg
-dot -Tsvg  graph4.gv > graph4.svg
-dot -Tsvg  graph5.gv > graph5.svg
-dot -Tsvg  graph6.gv > graph6.svg
-dot -Tsvg  graph7.gv > graph7.svg
-dot -Tsvg  graph8.gv > graph8.svg
-
