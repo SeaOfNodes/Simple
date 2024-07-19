7b2e1fa1d596217a214f7c5802c2778a0767c0be
Cliff Click <cliffc@acm.org>
2024-07-19 16:25:26 -0700

    minimal empty test so "make" doesnt complain

--- chapter12/src/test/java/com/seaofnodes/simple/Chapter12Test.java
+++ chapter12/src/test/java/com/seaofnodes/simple/Chapter12Test.java
@@ -8,5 +8,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Chapter12Test {
+    // A placeholder test used to rapidly rotate through fuzzer produced issues
+    @Test
+    public void testFuzzer() {
+        Parser parser = new Parser(
+"""
+return 0;
+""");
+        StopNode stop = parser.parse(false).iterate(true);
+        assertEquals("return 0;", stop.toString());
+        assertEquals(0L, Evaluator.evaluate(stop,  0));
+    }
 
 }
