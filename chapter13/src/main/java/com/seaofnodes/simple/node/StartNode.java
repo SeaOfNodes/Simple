71fcaf41eabdaffeab284a9c3f6528b249757202
dibyendumajumdar <mobile@majumdar.org.uk>
2024-08-05 23:20:49 +0100

    #92 chapter 13 from 12, splitting out ref fields in structs

---
+++ chapter13/src/main/java/com/seaofnodes/simple/node/StartNode.java
@@ -0,0 +1,85 @@
+package com.seaofnodes.simple.node;
+
+import com.seaofnodes.simple.Parser;
+import com.seaofnodes.simple.type.*;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * The Start node represents the start of the function.
+ *
+ * Start initially has 1 input (arg) from outside and the initial control.
+ * In ch10 we also add mem aliases as structs get defined; each field in struct
+ * adds a distinct alias to Start's tuple.
+ */
+public class StartNode extends CFGNode implements MultiNode {
+
+    // This field is NOT final, because the tuple expands as we add memory aliases
+    TypeTuple _args;
+
+    // The alias number of the first field in the struct.
+    //
+    public final HashMap<String,Integer> _aliasStarts;
+
+
+    public StartNode(Type[] args) {
+        super();
+        _args = TypeTuple.make(args);
+        _type = _args;
+        _aliasStarts = new HashMap<>();
+    }
+
+    /**
+     * Creates a projection for each of the struct's fields, using the field alias
+     * as the key.
+     */
+    public void addMemProj( TypeStruct ts, ScopeNode scope) {
+        int len = _args._types.length;
+        _aliasStarts.put(ts._name,len);
+
+        // resize the tuple's type array to include all fields of the struct
+        int max = len + ts._fields.length;
+        Type[] args = Arrays.copyOf(_args._types, max);
+
+        // The new members of the tuple get a mem type with an alias
+        for( int alias = len; alias < max; alias++ )
+            args[alias] = TypeMem.make(alias);
+        _type = _args = TypeTuple.make(args);
+        // For each of the fields we now add a mem projection.  Note that the
+        // alias matches the slot of the field in the tuple
+        for( int alias = len; alias < max; alias++ ) {
+            String name = Parser.memName(alias);
+            Node n = new ProjNode(this, alias, name).peephole();
+            scope.define(name, args[alias], n);
+        }
+    }
+
+    @Override
+    public String label() { return "Start"; }
+
+    @Override
+    StringBuilder _print1(StringBuilder sb, BitSet visited) {
+      return sb.append(label());
+    }
+
+    @Override public boolean isMultiHead() { return true; }
+    @Override public boolean blockHead() { return true; }
+
+    @Override
+    public TypeTuple compute() { return _args; }
+
+    @Override
+    public Node idealize() { return null; }
+
+    // No immediate dominator, and idepth==0
+    @Override public int idepth() { return 0; }
+    @Override public CFGNode idom(Node dep) { return null; }
+
+    @Override void _walkUnreach( BitSet visit, HashSet<CFGNode> unreach ) { }
+
+    @Override public int loopDepth() { return (_loopDepth=1); }
+
+    @Override public Node getBlockStart() { return this; }
+}
---
+++ chapter13/src/main/java/com/seaofnodes/simple/node/SubNode.java
@@ -0,0 +1,52 @@
+package com.seaofnodes.simple.node;
+
+import com.seaofnodes.simple.Parser;
+import com.seaofnodes.simple.type.Type;
+import com.seaofnodes.simple.type.TypeInteger;
+
+import java.util.BitSet;
+
+public class SubNode extends Node {
+    public SubNode(Node lhs, Node rhs) { super(null, lhs, rhs); }
+
+    @Override public String label() { return "Sub"; }
+
+    @Override public String glabel() { return "-"; }
+
+    @Override
+    StringBuilder _print1(StringBuilder sb, BitSet visited) {
+        in(1)._print0(sb.append("("), visited);
+        in(2)._print0(sb.append("-"), visited);
+        return sb.append(")");
+    }
+
+    @Override
+    public Type compute() {
+        if (in(1)._type instanceof TypeInteger i0 &&
+            in(2)._type instanceof TypeInteger i1) {
+            if (i0.isConstant() && i1.isConstant())
+                return TypeInteger.constant(i0.value()-i1.value());
+        }
+        return in(1)._type.meet(in(2)._type);
+    }
+
+    @Override
+    public Node idealize() {
+        // Sub of same is 0
+        if( in(1)==in(2) )
+            return Parser.ZERO;
+
+        // x - (-y) is x+y
+        if( in(2) instanceof MinusNode minus )
+            return new AddNode(in(1),minus.in(1));
+
+        // (-x) - y is -(x+y)
+        if( in(1) instanceof MinusNode minus )
+            return new MinusNode(new AddNode(minus.in(1),in(2)).peephole());
+
+        return null;
+    }
+
+    @Override Node copy(Node lhs, Node rhs) { return new SubNode(lhs,rhs); }
+    @Override Node copyF() { return new SubFNode(null,null); }
+}
