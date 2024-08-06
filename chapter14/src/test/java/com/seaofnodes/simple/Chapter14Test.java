c28c614491efd532935a87b476763c352bdddaea
dibyendumajumdar <mobile@majumdar.org.uk>
2024-08-06 13:17:14 +0100

    #98 Chapter 14 arrays

---
+++ chapter14/src/test/java/com/seaofnodes/simple/Chapter14Test.java
@@ -0,0 +1,23 @@
+package com.seaofnodes.simple;
+
+import com.seaofnodes.simple.evaluator.Evaluator;
+import com.seaofnodes.simple.node.StopNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class Chapter14Test {
+    // This is a just a copy of a test from ch12 - remove!
+    @Test
+    public void testToBeRemoved() {
+        Parser parser = new Parser(
+"""
+return 3.14;
+""");
+        StopNode stop = parser.parse(false).iterate(false);
+        assertEquals("return 3.14;", stop.toString());
+        assertEquals(3.14, Evaluator.evaluate(stop,  0));
+    }
+
+}
