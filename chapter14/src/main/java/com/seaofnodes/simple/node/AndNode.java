ace745a5b54a34231d8f3b55a1aa75c961f50892
Cliff Click <cliffc@acm.org>
2024-09-06 09:32:28 -0700

    Fix bugs
    AND on floats
    Shift-extend idiom smartly makes sharper int types.

--- chapter14/src/main/java/com/seaofnodes/simple/node/AndNode.java
+++ chapter14/src/main/java/com/seaofnodes/simple/node/AndNode.java
@@ -57,4 +57,9 @@ public class AndNode extends Node {
         return null;
     }
     @Override Node copy(Node lhs, Node rhs) { return new AndNode(lhs,rhs); }
+    @Override String err() {
+        if( !(in(1)._type instanceof TypeInteger) ) return "Cannot '&' " + in(1)._type;
+        if( !(in(2)._type instanceof TypeInteger) ) return "Cannot '&' " + in(2)._type;
+        return null;
+    }
 }
