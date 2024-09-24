6af26a247bf583f0a6a50f44539655367bf800bb
Cliff Click <cliffc@acm.org>
2024-09-24 09:26:07 -0700

    Address math folding

--- chapter15/src/main/java/com/seaofnodes/simple/node/ShlNode.java
+++ chapter15/src/main/java/com/seaofnodes/simple/node/ShlNode.java
@@ -1,8 +1,8 @@
 package com.seaofnodes.simple.node;
 
+import com.seaofnodes.simple.Parser;
 import com.seaofnodes.simple.type.Type;
 import com.seaofnodes.simple.type.TypeInteger;
-
 import java.util.BitSet;
 
 public class ShlNode extends Node {
@@ -33,11 +33,18 @@ public class ShlNode extends Node {
     public Node idealize() {
         Node lhs = in(1);
         Node rhs = in(2);
-        Type t2 = rhs._type;
 
-        // Shl of 0.
-        if( t2.isConstant() && t2 instanceof TypeInteger i && (i.value()&63)==0 )
-            return lhs;
+        if( rhs._type instanceof TypeInteger shl && shl.isConstant() ) {
+            // Shl of 0.
+            if( (shl.value()&63)==0 )
+                return lhs;
+            // (x + c) << i  =>  (x << i) + (c << i)
+            if( lhs instanceof AddNode add && add.in(2)._type instanceof TypeInteger c && c.isConstant() ) {
+                long sum = c.value() << shl.value();
+                if( Integer.MIN_VALUE <= sum  && sum <= Integer.MAX_VALUE )
+                    return new AddNode( new ShlNode(add.in(1),rhs).peephole(), Parser.con(sum) );
+            }
+        }
 
         // TODO: x << 3 << (y ? 1 : 2) ==> x << (y ? 4 : 5)
 
