c4c8ee708c6948dcfa41be61eb96e744d5c6d3f8
Cliff Click <cliffc@acm.org>
2024-10-18 08:47:29 -0700

    Missing init-store-after-new opt

--- chapter16/src/test/java/com/seaofnodes/simple/Chapter15Test.java
+++ chapter16/src/test/java/com/seaofnodes/simple/Chapter15Test.java
@@ -130,8 +130,9 @@ else {
 return rez;
 """);
         StopNode stop = parser.parse().iterate();
-        assertEquals("return Phi(Region117,1.2,Phi(Region114,2.3,.y));", stop.toString());
+        assertEquals("return Phi(Region117,1.2,Phi(Region114,2.3,3.14));", stop.toString());
         assertEquals(3.14, Evaluator.evaluate(stop, 0));
+        assertEquals(1.2 , Evaluator.evaluate(stop, 1));
     }
 
     @Ignore
