a8229dff7f625e882411f16857e7257045f1536e
Cliff Click <cliffc@acm.org>
2025-02-07 08:58:36 -0800

    Chapter 19: Instruction Selection and Portable Compilation
    Co-authored-by: Tamás Hadházy <85063808+Hels15@users.noreply.github.com>
    Co-authored-by: hadha <helsgithub15@gmail.com>
    Co-authored-by: godalming123 <68993177+godalming123@users.noreply.github.com>
    Co-authored-by: RobertObkircher <robert.obkircher@gmail.com>

--- src/main/java/com/seaofnodes/simple/node/XCtrlNode.java
+++ src/main/java/com/seaofnodes/simple/node/XCtrlNode.java
@@ -1,11 +1,12 @@
 package com.seaofnodes.simple.node;
 
+import com.seaofnodes.simple.CodeGen;
 import com.seaofnodes.simple.Parser;
 import com.seaofnodes.simple.type.Type;
 import java.util.BitSet;
 
 public class XCtrlNode extends CFGNode {
-    public XCtrlNode() { super(Parser.START); }
+    public XCtrlNode() { super(new Node[]{CodeGen.CODE._start}); }
     @Override public String label() { return "Xctrl"; }
     @Override StringBuilder _print1(StringBuilder sb, BitSet visited) { return sb.append("Xctrl"); }
     @Override public boolean isConst() { return true; }
---
+++ src/main/java/com/seaofnodes/simple/node/cpus/x86_64_v2/AndX86.java
@@ -0,0 +1,33 @@
+package com.seaofnodes.simple.node.cpus.x86_64_v2;
+
+import com.seaofnodes.simple.*;
+import com.seaofnodes.simple.node.*;
+import java.io.ByteArrayOutputStream;
+
+public class AndX86  extends MachConcreteNode implements MachNode{
+    AndX86(Node and) {
+        super(and);
+    }
+    // Register mask allowed on input i.
+    // This is the normal calling convention
+    @Override public RegMask regmap(int i) { assert i==1; return x86_64_v2.WMASK; }
+
+    @Override public RegMask outregmap() { return x86_64_v2.WMASK; }
+
+    // Output is same register as input#1
+    @Override public int twoAddress() { return 1; }
+
+    // Encoding is appended into the byte array; size is returned
+    @Override public int encoding(ByteArrayOutputStream bytes) {
+        throw Utils.TODO();
+    }
+
+    // General form
+    // General form: "and  dst & #imm"
+    @Override public void asm(CodeGen code, SB sb) {
+        sb.p(code.reg(this)).p(" = ").p(code.reg(in(1)));
+    }
+
+    @Override public String op() { return "and"; }
+}
+
