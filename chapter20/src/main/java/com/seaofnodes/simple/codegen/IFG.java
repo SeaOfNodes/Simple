0819eeda1132042d2ccab0c08cc0c6c2097650c2
Cliff Click <cliffc@acm.org>
2025-02-09 11:19:34 -0800

    Live Ranges U-F
    BuildLRG for Phi
    Add CmpFX86

--- chapter20/src/main/java/com/seaofnodes/simple/codegen/IFG.java
+++ chapter20/src/main/java/com/seaofnodes/simple/codegen/IFG.java
@@ -6,7 +6,8 @@ import com.seaofnodes.simple.node.CFGNode;
 
 // Interference Graph
 abstract public class IFG {
-    public static boolean build(int round, Ary<CFGNode> cfg) {
+    public static boolean build(int round, RegAlloc alloc ) {
+        System.out.println(alloc._code.asm());
         throw Utils.TODO();
     }
     public static boolean color(int round, CodeGen code) {
