273e6999bf8a52f5722a64278d99246a3d4ec68e
Cliff Click <cliffc@acm.org>
2025-02-16 11:31:41 -0800

    Update Chapter20Test.java

--- chapter20/src/test/java/com/seaofnodes/simple/Chapter20Test.java
+++ chapter20/src/test/java/com/seaofnodes/simple/Chapter20Test.java
@@ -71,48 +71,44 @@ return ary[1] * 1000 + ary[3]; // 1 * 1000 + 6
 
     @Test
     public void testArray2() {
-        CodeGen code = new CodeGen(
-                """
-                        struct String {
-                            u8[] cs;
-                            int _hashCode;
-                        };
-                        
-                        val equals = { String self, String s ->
-                            if( self == s ) return true;
-                            if( self.cs# != s.cs# ) return false;
-                            for( int i=0; i< self.cs#; i++ )
-                                if( self.cs[i] != s.cs[i] )
-                                    return false;
-                            return true;
-                        };
-                        
-                        val hashCode = { String self ->
-                            self._hashCode
-                            ?  self._hashCode
-                            : (self._hashCode = _hashCodeString(self));
-                        };
-                        
-                        val _hashCodeString = { String self ->
-                            int hash=0;
-                            if( self.cs ) {
-                                for( int i=0; i< self.cs#; i++ )
-                                    hash = hash*31 + self.cs[i];
-                            }
-                            if( !hash ) hash = 123456789;
-                            return hash;
-                        };
-                        
-                        String !s = new String { cs = new u8[17]; };
-                        s.cs[0] =  67; // C
-                        s.cs[1] = 108; // l
-                        hashCode(s);"""
-        );
+        CodeGen code = new CodeGen("""
+struct String {
+    u8[] cs;
+    int _hashCode;
+};
+
+val equals = { String self, String s ->
+    if( self == s ) return true;
+    if( self.cs# != s.cs# ) return false;
+    for( int i=0; i< self.cs#; i++ )
+        if( self.cs[i] != s.cs[i] )
+            return false;
+    return true;
+};
+
+val hashCode = { String self ->
+    self._hashCode
+    ?  self._hashCode
+    : (self._hashCode = _hashCodeString(self));
+};
+
+val _hashCodeString = { String self ->
+    int hash=0;
+    if( self.cs ) {
+        for( int i=0; i< self.cs#; i++ )
+            hash = hash*31 + self.cs[i];
+    }
+    if( !hash ) hash = 123456789;
+    return hash;
+};
+
+String !s = new String { cs = new u8[17]; };
+s.cs[0] =  67; // C
+s.cs[1] = 108; // l
+hashCode(s);
+""");
         code.parse().opto().typeCheck().instSelect("x86_64_v2", "SystemV").GCM().localSched();
         assertEquals("Stop[ return Phi(Region,123456789,Phi(Loop,0,.[])); return Phi(Region,1,0,0,1); ]", code.print());
-        SB sb = new SB();
-        ASMPrinter.print(sb, code);
-        System.out.print(sb);
     }
 
     @Test
@@ -126,7 +122,6 @@ return ary[1] * 1000 + ary[3]; // 1 * 1000 + 6
         );
         code.parse().opto().typeCheck().instSelect("x86_64_v2", "SystemV").GCM().localSched();
         assertEquals("return Phi(Region,(lea, ---,.x),-1);", code.print());
-        code.print();
     }
 
 }
