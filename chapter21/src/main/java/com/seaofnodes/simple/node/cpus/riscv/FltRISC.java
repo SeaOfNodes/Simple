442b5060a7f5857cfdedcc69384d90fd5fed9036
Cliff Click <cliffc@acm.org>
2025-03-19 09:54:45 -0700

    constant pool

--- chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/FltRISC.java
+++ chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/FltRISC.java
@@ -7,9 +7,9 @@ import com.seaofnodes.simple.node.ConstantNode;
 import com.seaofnodes.simple.node.MachNode;
 import com.seaofnodes.simple.type.TypeFloat;
 
-public class FltRISC extends ConstantNode implements MachNode{
+public class FltRISC extends ConstantNode implements MachNode, RIPRelSize {
     FltRISC(ConstantNode con) { super(con); }
-    @Override public String op() { return "flw"; }
+    @Override public String op() { return "ld8"; }
     @Override public RegMask regmap(int i) { return null; }
     @Override public RegMask outregmap() { return riscv.FMASK; }
     @Override public boolean isClone() { return true; }
@@ -24,8 +24,21 @@ public class FltRISC extends ConstantNode implements MachNode{
         enc.add4(riscv.i_type(0x07, dst, 0x03, dst, 0));
     }
 
+    // Delta is from opcode start.
+    // TODO: always size 8?
+    @Override public byte encSize(int delta) { return 8;  }
+
+    // Delta is from opcode start
+    @Override public void patch( Encoding enc, int opStart, int opLen, int delta ) {
+        short dst = (short)(enc.reg(this) - riscv.F_OFFSET);
+        // AUIPC dst,#hi20_constant_pool
+        enc.patch4(opStart  , riscv.u_type(0b0010111, dst, delta));
+        // Load dst,[dst+#low12_constant_pool]
+        enc.patch4(opStart+4, riscv.i_type(0x07, dst, 0x03, dst, delta));
+    }
+
     @Override public void asm(CodeGen code, SB sb) {
-        _con.print(sb.p(code.reg(this)).p(" #"));
+        _con.print(sb.p(code.reg(this)).p(" = #"));
     }
 
 }
