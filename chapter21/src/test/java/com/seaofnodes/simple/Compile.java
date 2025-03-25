5fd3508bd3c45ed83757b47caa7a24361bec71cb
XmiliaH <45106915+XmiliaH@users.noreply.github.com>
2025-03-25 18:44:41 +0100

    Call Simple function from C (#179)
    * Call simple function from c
    
    * Small fix
    
    * Use different test values

--- chapter21/src/test/java/com/seaofnodes/simple/Compile.java
+++ chapter21/src/test/java/com/seaofnodes/simple/Compile.java
@@ -1,27 +1,68 @@
 package com.seaofnodes.simple;
 
 import com.seaofnodes.simple.codegen.CodeGen;
+import com.seaofnodes.simple.node.FunNode;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
 
 import static com.seaofnodes.simple.Main.PORTS;
 
 public class Compile {
 
+    private static Process run(String simple, String c) throws IOException, InterruptedException {
+        boolean USE_WSL = System.getProperty("os.name").startsWith("Windows");
+        CodeGen code = new CodeGen(simple);
+        code.parse().opto().typeCheck().instSelect(PORTS,"x86_64_v2", "SystemV").GCM().localSched().regAlloc().encode();
+        if (c != null) for (var n:code._start._outputs) if (n instanceof FunNode f && f._name.equals("main")) f._name = "simple_main";
+        code.exportELF("test.o");
+        var params = new ArrayList<String>();
+        if (USE_WSL) params.add("wsl.exe");
+        params.add("gcc");
+        params.add("-o");
+        params.add("test");
+        if (c != null) {
+            Files.writeString(Path.of("test.c"), c);
+            params.add("test.c");
+        }
+        params.add("test.o");
+        Process p = Runtime.getRuntime().exec(params.toArray(String[]::new));
+        assertEquals(0, p.waitFor());
+        return Runtime.getRuntime().exec(USE_WSL ? new String[]{"wsl.exe", "./test"}:new String[]{"./test"});
+    }
+
     @Test
     @Ignore
     public void testCompile() throws IOException, InterruptedException {
-        boolean USE_WSL = System.getProperty("os.name").startsWith("Windows");
-        CodeGen code = new CodeGen("return 13;");
-        code.parse().opto().typeCheck().instSelect(PORTS,"x86_64_v2", "SystemV").GCM().localSched().regAlloc().encode();
-        code.exportELF("test.o");
-        Process p = Runtime.getRuntime().exec(USE_WSL ? new String[]{"wsl.exe", "gcc", "-o", "test", "test.o"}:new String[]{"gcc", "-o", "test", "test.o"});
-        assert p.waitFor() == 0;
-        p = Runtime.getRuntime().exec(USE_WSL ? new String[]{"wsl.exe", "./test"}:new String[]{"./test"});
-        System.out.println(p.waitFor());
-        assert p.waitFor() == 13;
+        var p = run("""
+                val testMy = { flt x ->
+                    flt guess = x;
+                    while( 1 ) {
+                        flt next = (x/guess + guess)/2;
+                        if( next == guess ) return guess;
+                        guess = next;
+                    }
+                };
+                ""","""
+                
+                #include <stdio.h>
+                
+                extern double testMy(double);
+                
+                int main(int argc, char** argv) {
+                    printf("%f\\n", testMy(2.0));
+                    return 0;
+                }
+                
+                """);
+        assertEquals(0, p.waitFor());
+        assertEquals("1.414214\n", new String(p.getInputStream().readAllBytes(), StandardCharsets.UTF_8));
     }
 
 }
