1aa6c9503ce50e41b58a42bfcfde7fc28d302ca2
Hadházy Tamás <helsgithub15@gmail.com>
2025-04-01 20:16:49 +0100

    revert changes

--- chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/SetIRISC.java
+++ chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/SetIRISC.java
@@ -6,12 +6,12 @@ import com.seaofnodes.simple.node.Node;
 // corresponds to slti,sltiu
 public class SetIRISC extends ImmRISC {
     final boolean _unsigned;    // slti vs sltiu
-    SetIRISC( Node src, int imm12, boolean unsigned ) {
+    public SetIRISC( Node src, int imm12, boolean unsigned ) {
         super(src,imm12);
         _unsigned = unsigned;
     }
-    @Override public String op() { return "setlt" + (_unsigned ? "u":""); }
+    @Override public String op() { return "slt" + (_unsigned ? "u":"") + "i"; }
     @Override public String glabel() { return  "<"+ (_unsigned ? "u":""); }
-    @Override int opcode() { return riscv.OP_IMM; }
-    @Override int func3() { return _unsigned ? 3: 2; }
+    @Override int opcode() { return 0b0010011; }
+    @Override int func3() { return _unsigned ? 0b011 : 0b010; }
 }
--- chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/SetRISC.java
+++ chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/SetRISC.java
@@ -1,34 +1,18 @@
 package com.seaofnodes.simple.node.cpus.riscv;
 
 import com.seaofnodes.simple.SB;
-import com.seaofnodes.simple.Utils;
 import com.seaofnodes.simple.codegen.*;
 import com.seaofnodes.simple.node.*;
 
 // corresponds to slt (and NOT sltu, slti, sltiu)
 public class SetRISC extends MachConcreteNode implements MachNode {
-    final String _bop;
-    SetRISC( Node cmp, String bop) { super(cmp); _bop = bop;}
-    @Override public String op() { return "set" ; }
+    final boolean _unsigned;
+    public SetRISC( Node cmp, boolean unsigned ) { super(cmp); _unsigned=unsigned; }
+    @Override public String op() { return "slt" + (_unsigned ? "u":""); }
     @Override public RegMask regmap(int i) { return riscv.RMASK; }
     @Override public RegMask outregmap() { return riscv.WMASK; }
     @Override public void encoding( Encoding enc ) {
-        short dst = enc.reg(this );
-        short src = enc.reg(in(1));
-        switch(_bop) {
-            case "==":
-                // sltiu rd, rs, 1
-                enc.add4(riscv.i_type(riscv.OP_IMM, dst, 3, src,  1));
-                break;
-            case "<":
-                riscv.r_type(enc,this,2,0);
-                break;
-            case "<=":
-                riscv.r_type(enc,this,2,0);
-                enc.add4(riscv.i_type(riscv.OP_IMM, src, 3, src,  1));
-                break;
-            default: throw Utils.TODO();
-        }
+        riscv.r_type(enc,this,_unsigned ? 0b011 : 0x010,0);
     }
     @Override public void asm(CodeGen code, SB sb) {
         sb.p(code.reg(this)).p(" = ").p(code.reg(in(1))).p(" < ").p(code.reg(in(2)));
