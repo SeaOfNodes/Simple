ea737ed0a49b091478e954e125a58a1842a89e5f
Darrick Wiebe <darrickw@gmail.com>
2025-04-01 01:53:31 -0400

    Get SLT[IU]* working (#192)

--- chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/SetRISC.java
+++ chapter21/src/main/java/com/seaofnodes/simple/node/cpus/riscv/SetRISC.java
@@ -6,11 +6,14 @@ import com.seaofnodes.simple.node.*;
 
 // corresponds to slt (and NOT sltu, slti, sltiu)
 public class SetRISC extends MachConcreteNode implements MachNode {
-    SetRISC( Node cmp ) { super(cmp); }
-    @Override public String op() { return "slt"; }
+    final boolean _unsigned;
+    public SetRISC( Node cmp, boolean unsigned ) { super(cmp); _unsigned=unsigned; }
+    @Override public String op() { return "slt" + (_unsigned ? "u":""); }
     @Override public RegMask regmap(int i) { return riscv.RMASK; }
     @Override public RegMask outregmap() { return riscv.WMASK; }
-    @Override public void encoding( Encoding enc ) { riscv.r_type(enc,this,2,0); }
+    @Override public void encoding( Encoding enc ) {
+        riscv.r_type(enc,this,_unsigned ? 0b011 : 0x010,0);
+    }
     @Override public void asm(CodeGen code, SB sb) {
         sb.p(code.reg(this)).p(" = ").p(code.reg(in(1))).p(" < ").p(code.reg(in(2)));
     }
