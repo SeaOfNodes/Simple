14c5e1cea1eb3307f9b1ff516f959730dc45a30a
Cliff Click <cliffc@acm.org>
2025-11-25 12:10:54 -0800

    Force emacs LF endings

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 (
  (dired-mode . ((c-basic-offset . 4)))
- (java-mode  . ((c-basic-offset . 4)))
+ (java-mode  . ((c-basic-offset . 4) (eval . (set-buffer-file-coding-system 'unix))) )
 )
